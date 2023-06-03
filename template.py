49e6e0aaa9071b89fc7752c4247497c58ba960a7
Sourav-Kumar-Khan <khansourav1999@gmail.com>
2023-06-03 09:59:35 +0530

    add the create dir command

--- template.py
+++ template.py
@@ -32,3 +32,16 @@ list_of_files = [
 for file_path in list_of_files:
     file_path = Path(file_path)
     filedir, filename = os.path.split(file_path)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for the file '{filename}")
+
+    if (not os.path.exists(file_path) or os.path.getsize(file_path)==0):
+        with open(file_path, 'w') as f:
+            pass
+            logging.info(f"Created empty file: {file_path}")
+    
+    else:
+            logging.info(f"File already exists: {file_path}")
+            
